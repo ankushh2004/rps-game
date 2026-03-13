2a5a05e0295b374d7745c9e4c771a46b6d5007a5
ankushh2004 <asanodiya@ancilar.com>
2026-03-13 07:57:26 +0530

    feat: configure ethers provider and signer & declare global type for ethereum

---
+++ src/lib/ethers.ts
@@ -0,0 +1,19 @@
+import { ethers } from "ethers";
+
+// Function to get the provider from MetaMask
+export const getProvider = () => {
+  if (!window.ethereum) {
+    throw new Error("MetaMask is not installed");
+  }
+
+  return new ethers.providers.Web3Provider(window.ethereum);
+};
+
+// Function to get the signer from the provider
+export const getSigner = async () => {
+  const provider = getProvider();
+  await provider.send("eth_requestAccounts", []);
+  const signer = provider.getSigner();
+
+  return signer;
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,10 @@
+// declaring global interface for window.ethereum
+declare global{
+  interface Window{
+    ethereum?: any;
+  }
+}
+
 // wallet slice types
 export interface WalletState {
   address: string;
