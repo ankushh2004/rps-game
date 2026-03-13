abf27def0b5f76711ddf0732921041edeadc788f
ankushh2004 <asanodiya@ancilar.com>
2026-03-13 11:22:46 +0530

    feat: setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Provider } from "react-redux";
 import Login from "./pages/Login";
+import { Route, BrowserRouter as Router, Routes } from "react-router";
+import store from "./store";
 
 function App() {
-  return <Login />;
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
 }
 
 export default App;
